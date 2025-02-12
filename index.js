203f34fe3494093a94aa94a161539c2dc5b96012
Aki19-2000 <72126254+Aki19-2000@users.noreply.github.com>
2025-02-12 08:45:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,5 @@
 const core = require('@actions/core');
 
-// Read inputs via environment variables (Docker run passes them as ENV)
-const input1 = process.env.INPUT1; 
-const input2 = process.env.INPUT2;
-
-console.log(`Environment variable Input 1: ${input1}`);
-console.log(`Environment variable Input 2: ${input2}`);
-
-// Optionally, you can still use GitHub Actions' core API for inputs
 const actionInput1 = core.getInput('input1');
 const actionInput2 = core.getInput('input2');
 
